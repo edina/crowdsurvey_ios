b6a7eb6dc92bea26c5aeb645eb6b8747548a8255
Colin Gormley <colin.gormley@ed.ac.uk>
2015-12-03 12:51:11 +0000

    Initial Commit

---
+++ FieldTripOpen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FieldTripOpen
+//
+//  Created by Colin Gormley on 03/12/2015.
+//  Copyright © 2015 Edina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
