c2cf1c19d34afe22bbba212e00dbdf49a4b2e1fa
Colin Gormley <colin.gormley@ed.ac.uk>
2015-12-04 11:28:13 +0000

    Fixes #6: Added navbar and tabbar controllers

---
+++ FieldTripOpen/Controllers/SurveyViewController.h
@@ -0,0 +1,13 @@
+//
+//  SurveyViewController.h
+//  FieldTripOpen
+//
+//  Created by Colin Gormley on 04/12/2015.
+//  Copyright © 2015 Edina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SurveyViewController : UIViewController
+
+@end
---
+++ FieldTripOpen/Controllers/SurveyViewController.m
@@ -0,0 +1,37 @@
+//
+//  SurveyViewController.m
+//  FieldTripOpen
+//
+//  Created by Colin Gormley on 04/12/2015.
+//  Copyright © 2015 Edina. All rights reserved.
+//
+
+#import "SurveyViewController.h"
+
+@interface SurveyViewController ()
+
+@end
+
+@implementation SurveyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
