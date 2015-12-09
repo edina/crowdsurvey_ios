2490e37ba06ad8c92e8e732bc59f5b89b1bbef1a
Ian Fieldhouse <me@ianfieldhouse.com>
2015-12-09 16:31:50 +0000

    Issue #7: Proof of concept - get JSON, save the retrieve from couchdb

--- FieldTripOpen/SurveyFactory.m
+++ FieldTripOpen/SurveyFactory.m
@@ -9,17 +9,19 @@
 #import "SurveyFactory.h"
 #import "CBObjects.h"
 #import <CouchbaseLite/CouchbaseLite.h>
-#import "RKDynamicMapping.h"
-#import "RKMIMETypeSerialization.h"
-#import "RKObjectMapping.h"
-#import "RKObjectMappingOperationDataSource.h"
-#import "RKObjectRequestOperation.h"
-#import "RKRelationshipMapping.h"
-#import "RKResponseDescriptor.h"
+#import "AFNetworking.h"
+//#import "RKDynamicMapping.h"
+//#import "RKMIMETypeSerialization.h"
+//#import "RKObjectMapping.h"
+//#import "RKObjectMappingOperationDataSource.h"
+//#import "RKObjectRequestOperation.h"
+//#import "RKRelationshipMapping.h"
+//#import "RKResponseDescriptor.h"
 
 @interface SurveyFactory ()
 
 @property (strong, nonatomic) Survey *survey;
+@property (strong, nonatomic) NSString *jsonString;
 
 @end
 
@@ -34,45 +36,75 @@
 }
 
 
--(void) createSurveyFromUrl:(NSURL *) url withCompletion:(void (^)(Survey *survey))completion
+//-(void) createSurveyFromUrl:(NSURL *) url withCompletion:(void (^)(Survey *survey))completion
+//{
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    
+//    RKObjectMapping* surveyMapping = [RKObjectMapping mappingForClass:[Survey class]];
+//    [surveyMapping addAttributeMappingsFromDictionary:@{
+//                                                        @"title": @"title",
+//                                                        @"geoms": @"geoms"
+//                                                        }];
+//    
+//    RKObjectMapping* fieldMapping = [RKObjectMapping mappingForClass:[Field class]];
+//    [fieldMapping addAttributeMappingsFromDictionary:@{
+//                                                       @"id": @"fieldId",
+//                                                       @"type": @"type",
+//                                                       @"label": @"label",
+//                                                       @"required": @"required",
+//                                                       @"persistent": @"persistent",
+//                                                       @"properties": @"properties"
+//                                                       }];
+//    
+//    [surveyMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"fields"
+//                                                                                  toKeyPath:@"fields"
+//                                                                                withMapping:fieldMapping]];
+//    
+//    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:surveyMapping method:RKRequestMethodAny pathPattern:nil keyPath:nil statusCodes:nil];
+//    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request responseDescriptors:@[responseDescriptor]];
+//    [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *result) {
+//        self.survey = [result firstObject];
+//        NSLog(@"Mapped the Survey: %@", self.survey);
+//        for (Field *field in self.survey.fields){
+//            NSString *fid = field.fieldId;
+//            NSString *type = field.type;
+//            NSLog(@"%@", fid);
+//            NSLog(@"%@", type);
+//        }
+//        completion(self.survey);
+//        
+//    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+//        NSLog(@"Failed with error: %@", [error localizedDescription]);
+//    }];
+//    
+//    [operation start];
+//}
+
+-(void) createSurveyFromUrl:(NSURL *)url withCompletion:(void (^)(Survey *))completion
 {
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
-    RKObjectMapping* surveyMapping = [RKObjectMapping mappingForClass:[Survey class]];
-    [surveyMapping addAttributeMappingsFromDictionary:@{
-                                                        @"title": @"title",
-                                                        @"geoms": @"geoms"
-                                                        }];
-    
-    RKObjectMapping* fieldMapping = [RKObjectMapping mappingForClass:[Field class]];
-    [fieldMapping addAttributeMappingsFromDictionary:@{
-                                                       @"id": @"fieldId",
-                                                       @"type": @"type",
-                                                       @"label": @"label",
-                                                       @"required": @"required",
-                                                       @"persistent": @"persistent",
-                                                       @"properties": @"properties"
-                                                       }];
-    
-    [surveyMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"fields"
-                                                                                  toKeyPath:@"fields"
-                                                                                withMapping:fieldMapping]];
-    
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:surveyMapping method:RKRequestMethodAny pathPattern:nil keyPath:nil statusCodes:nil];
-    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request responseDescriptors:@[responseDescriptor]];
-    [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *result) {
-        self.survey = [result firstObject];
-        NSLog(@"Mapped the Survey: %@", self.survey);
-        for (Field *field in self.survey.fields){
-            NSString *fid = field.fieldId;
-            NSString *type = field.type;
-            NSLog(@"%@", fid);
-            NSLog(@"%@", type);
-        }
-        completion(self.survey);
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        // create survey in CouchDB
+        NSString *documentId = [self createSurveyDocument:JSON];
+        CBLDocument *doc = [CBObjects.sharedInstance.database documentWithID:documentId];
+        NSMutableDictionary *docContent = [doc.properties mutableCopy];
         
-    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-        NSLog(@"Failed with error: %@", [error localizedDescription]);
+        // convert JSON to NSString
+        NSError *error;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:docContent
+                                                           options:NSJSONWritingPrettyPrinted
+                                                             error:&error];
+        if (! jsonData) {
+            NSLog(@"Got an error: %@", error);
+        } else {
+            self.jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        }
+        
+        NSLog(@"Done");
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"%ld", (long)[response statusCode]);
+        NSLog(@"Failed to get JSON");
     }];
     
     [operation start];
@@ -85,26 +117,33 @@
 //
 //[self createOrderedByDateView];
 
-//
-//
-//# pragma mark - CouchDB
-//
-//// creates the Document
-//- (NSString *)createSurveyDocument: (NSDictionary *)survey {
-//    
-//    // 1. Create an empty document
-//    CBLDocument *doc = [CBObjects.sharedInstance.database createDocument];
-//    // 2. Save the ID of the new document
-//    NSString *docID = doc.documentID;
-//    // 3. Write the document to the database
-//    NSError *error;
-//    CBLRevision *newRevision = [doc putProperties: survey error:&error];
-//    if (newRevision) {
-//        NSLog(@"Document created and written to database, ID = %@", docID);
-//    }
-//    return docID;
-//}
-//
+
+
+# pragma mark - CouchDB
+
+// creates the Document
+- (NSString *)createSurveyDocument: (NSDictionary *)surveyJson {
+    
+    // 1. Create an empty document
+    CBLDocument *doc = [CBObjects.sharedInstance.database createDocument];
+    // 2. Save the ID of the new document
+    NSString *docID = doc.documentID;
+    // 3. Write the document to the database
+    NSError *error;
+    CBLRevision *newRevision = [doc putProperties: surveyJson error:&error];
+    if (newRevision) {
+        NSLog(@"Document created and written to database, ID = %@", docID);
+    }
+    return docID;
+}
+
+- (CBLDocument *)getDocumentFromDatabase: (CBLDatabase *) database
+                              withDocumentId: (NSString *) documentId
+{
+    CBLDocument *document = [database documentWithID: documentId];
+    return document;
+}
+
 //- (CBLView *)getView {
 //    CBLDatabase* database = [CBObjects sharedInstance].database;
 //    return [database viewNamed:@"byDate"];
