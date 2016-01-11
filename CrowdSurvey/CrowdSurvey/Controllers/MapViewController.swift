90918fbf221f923f9d2df353ac22fea4637419a1
Colin Gormley <colin.gormley@ed.ac.uk>
2016-01-11 16:05:36 +0000

    Issue #11: Initial attempt at a map view.

---
+++ CrowdSurvey/CrowdSurvey/Controllers/MapViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MapViewController.swift
+//  CrowdSurvey
+//
+//  Created by Colin Gormley on 11/01/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            // Set delegate for Map
+            mapView.delegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+    
+    // MARK: - MKMapViewDelegate
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
