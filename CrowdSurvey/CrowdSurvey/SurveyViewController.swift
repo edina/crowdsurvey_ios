87e057cfa0e23999faf3816f0b198443dc81fcd0
Colin Gormley <colin.gormley@ed.ac.uk>
2016-01-12 10:41:21 +0000

    Issue #11: Added survey controller skeleton

---
+++ CrowdSurvey/CrowdSurvey/SurveyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SurveyViewController.swift
+//  CrowdSurvey
+//
+//  Created by Colin Gormley on 12/01/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
