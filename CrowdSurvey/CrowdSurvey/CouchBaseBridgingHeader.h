c67f8b761c8a14094a96c6039dd6875d3a1efb3b
Colin Gormley <colin.gormley@ed.ac.uk>
2016-01-12 16:31:00 +0000

    Issue #12: Added Couchbase Framework and bridging header

---
+++ CrowdSurvey/CrowdSurvey/CouchBaseBridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  CouchBaseBridgingHeader.h
+//  CrowdSurvey
+//
+//  Created by Colin Gormley on 12/01/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+#ifndef CouchBaseBridgingHeader_h
+#define CouchBaseBridgingHeader_h
+
+
+#endif /* CouchBaseBridgingHeader_h */
--- CrowdSurvey/CrowdSurvey/SurveyViewController.swift
+++ CrowdSurvey/CrowdSurvey/SurveyViewController.swift
@@ -13,13 +13,12 @@ class SurveyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var manager = CBLManager.sharedInstance()       
+    
+      
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     /*
