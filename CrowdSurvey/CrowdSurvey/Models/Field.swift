c1c6ab2dfc42eed804c701a3673c507efd668027
Colin Gormley <colin.gormley@ed.ac.uk>
2016-01-15 14:27:58 +0000

    edina/crowdsurvey#15: Added separate methods for dealing with radio, text fields etc
    Implementation is incomplete at present

--- CrowdSurvey/CrowdSurvey/Models/Field.swift
+++ CrowdSurvey/CrowdSurvey/Models/Field.swift
@@ -24,6 +24,19 @@ class Field: Mappable {
             return Mapper().toJSONString(self, prettyPrint: true)!
         }
     }
+        
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let Text = "text"
+        static let Checkbox = "checkbox"
+        static let Radio = "radio"
+        static let Image = "image"
+        static let Select = "select"
+        static let Dtree = "dtree"
+        static let Warning = "warning"
+        static let Range = "range"
+    }
     
     // MARK: - ObjectMapper
     
@@ -44,31 +57,90 @@ class Field: Mappable {
     
     // MARK: - Form
     
-    
-    func appendToForm(var form: Form){
+    // Detect which kind of form element to construct
+    func appendToForm(form: Form){
         
         switch (type!)
         {
-        case "text":
+        case Constants.Text:
             print("text field")
+            addTextToForm(form)
             
-            // Create text field
-            form.last!   <<< TextRow () {
-                $0.title = label!
-                $0.placeholder = ""
-            }
-            
-        case "radio":
+        case Constants.Radio:
             print("radio field")
+            addRadioToForm(form)
             
-        case "checkbox":
+        case Constants.Image:
+            
+            print("image field")
+        case Constants.Dtree:
+            
+            print("dtree field")
+        case Constants.Warning:
+            
+            print("warning field")
+        case Constants.Select:
+            
+            print("Select field")
+        case Constants.Range:
+            
+            print("range field")
+        case Constants.Checkbox:
+            
             print("checkbox field")
-            form.last! <<< CheckRow() {
-                $0.title = label!
-                $0.value = false
-            }
+            addCheckBoxToForm(form)
+            
         default:
             print("default")
         }
     }
+    
+    // Add a simple text field
+    func addTextToForm(var form: Form){
+        //            form +++= Section(label!)
+        
+        
+        form +++= LabelRow () {
+            $0.title = label!
+            $0.value = ""
+            $0.cell.textLabel?.numberOfLines=0
+        }
+        
+        // Create text field
+        form.last!   <<< TextRow () {
+            $0.title = ""
+            $0.placeholder = ""
+        }
+    }
+    
+    func addRadioToForm(var form: Form){
+        //            form +++= Section(label!)
+        
+        form +++= LabelRow () {
+            $0.title = label!
+            $0.value = ""
+            $0.cell.textLabel?.numberOfLines=0
+        }
+        
+        if let options = properties!["options"] {
+            print("The options are \(options).")
+            print( "String(options.dynamicType) -> \(options.dynamicType)")
+            form.last!   <<< ActionSheetRow<String>() {
+                $0.title = ""
+                $0.selectorTitle = ""
+                $0.options = ["Diego Forlán", "Edinson Cavani", "Diego Lugano", "Luis Suarez"]
+                $0.value = ""
+                $0.cell.textLabel?.numberOfLines=0
+            }
+        }
+    }
+    
+    func addCheckBoxToForm(var form: Form){
+        form +++= Section(label!)
+        form.last! <<< CheckRow() {
+            $0.title = ""
+            $0.value = false
+        }
+    }
+    
 }
