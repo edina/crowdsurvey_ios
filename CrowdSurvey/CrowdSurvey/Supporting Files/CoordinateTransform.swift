44980b1c9ed7c0dc64235062451a920225778cd1
Ian Fieldhouse <me@ianfieldhouse.com>
2016-03-15 12:08:08 +0000

    edina/crowdsurvey#53: Add coordinate transform class for ObjectMapper

---
+++ CrowdSurvey/CrowdSurvey/Supporting Files/CoordinateTransform.swift
@@ -0,0 +1,34 @@
+//
+//  CoordinateTransform.swift
+//  CrowdSurvey
+//
+//  Created by Ian Fieldhouse on 15/03/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import ObjectMapper
+
+public class CoordinateTransform: TransformType {
+    public typealias Object = CLLocationCoordinate2D
+    public typealias JSON = Array<Double>
+    
+    public init() {}
+    
+    public func transformFromJSON(value: AnyObject?) -> CLLocationCoordinate2D? {
+        if let coordinateString = value {
+            guard coordinateString.count >= 2 else { return nil }
+            guard let latitude = coordinateString[1] as? Double, longitude = coordinateString[0] as? Double else { return nil }
+            return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        }
+        return nil
+    }
+    
+    public func transformToJSON(value: CLLocationCoordinate2D?) -> Array<Double>? {
+        if let coordinate = value {
+            return [coordinate.longitude, coordinate.latitude]
+        }
+        return nil
+    }
+}
