3e142c3389d52d209a5649b73cc3a4d422529bbc
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-12 15:28:47 +0100

    edina/crowdsurvey_ios#13: Added button to mapView

--- DFS/DFS/Controllers/MapViewController.swift
+++ DFS/DFS/Controllers/MapViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    // MARK: Variables
+    
+    
+    @IBOutlet weak var addPhotoButton: UIButton!{
+        didSet{
+            addPhotoButton.layer.cornerRadius = 30
+            addPhotoButton.layer.shadowColor = UIColor.blackColor().CGColor
+            addPhotoButton.layer.shadowOffset = CGSizeMake(2, 2)
+            addPhotoButton.layer.shadowRadius = 5
+            addPhotoButton.layer.shadowOpacity = 0.5
+            addPhotoButton.setTitleColor(Constants.Colour.LightBlue, forState: UIControlState.Normal)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
--- DFS/DFS/Utils/Constants.swift
+++ DFS/DFS/Utils/Constants.swift
@@ -10,6 +10,10 @@ import Foundation
 import UIKit
 
 struct Constants {
+    struct Colour {
+        static let DarkBlue = UIColor(red: 24/255.0, green:21/255.0, blue:76/255.0, alpha: 1.0)
+        static let LightBlue = UIColor(red: 34/255.0, green:31/255.0, blue:128/255.0, alpha: 1.0)
+    }
     struct SegueIDs {
         static let addPhoto = "addPhoto"
     }
