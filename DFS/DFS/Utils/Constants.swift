6bcd57ca892690cd0177323c18fff4c893789510
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-12 13:56:59 +0100

    edina/crowdsurvey_ios#13: Added pod file and constants class

---
+++ DFS/DFS/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  DFS
+//
+//  Created by Colin Gormley on 12/05/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct SegueIDs {
+        static let addPhoto = "addPhoto"
+    }
+}
