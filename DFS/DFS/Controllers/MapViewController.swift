d5038e89b3dfaff91ae177eaa36ae44342f42dff
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-12 14:07:51 +0100

    edina/crowdsurvey_ios#13: Added MapViewController

--- DFS/DFS/Controllers/MapViewController.swift
+++ DFS/DFS/Controllers/MapViewController.swift
@@ -6,8 +6,13 @@
 //  Copyright © 2016 Edina. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class MapViewController: UIViewController {
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
 }
