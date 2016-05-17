5a9b343445ebad5c02c935ae542ca8ab81f8e75b
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-17 11:54:24 +0100

    edina/crowdsurvey_ios#18: Hooked up camera and gallery buttons.  Not storing image yet.

--- DFS/DFS/PhotoViewController.swift
+++ DFS/DFS/PhotoViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
     
-
- 
+//    var imagePicker = UIImagePickerController?()
+    
     @IBOutlet weak var closeButton: UIButton!{
         didSet{
             closeButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4))
@@ -25,7 +25,68 @@ class PhotoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-// dismissViewControllerAnimated(true, completion: nil)
+//        imagePicker?.delegate=self
+
+    }
+    
+    @IBAction func goToCameraRoll(sender: AnyObject) {
+        self.cameraRoll()
+    }
+
+    @IBAction func takePhoto(sender: AnyObject) {
+        self.openCamera()
+    }
+    
+    func openCamera()
+    {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            //load the camera interface
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }else{
+            //no camera available
+            let alert = UIAlertController(title: "Error", message: "There is no camera available", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .Default, handler: {(alertAction)in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func cameraRoll() {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.PhotoLibrary
+         
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true,
+                                       completion: nil)
+        }
+
+    }
+    
+    
+    
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]){
+       
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            picker.dismissViewControllerAnimated(true, completion: nil)
+            //Implement if allowing user to edit the selected image
+            //let editedImage = info.objectForKey("UIImagePickerControllerEditedImage") as UIImage
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
