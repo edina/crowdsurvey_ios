302143ee618dbd69c142ab0ae6251dadfd7bab2a
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-17 14:30:25 +0100

    edina/crowdsurvey_ios#18: Added basic UICollectionView for displaying photos

---
+++ DFS/DFS/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  DFS
+//
+//  Created by Colin Gormley on 17/05/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func layoutSubviews() {
+        
+    }
+}
--- DFS/DFS/PhotoViewController.swift
+++ DFS/DFS/PhotoViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
     
-//    var imagePicker = UIImagePickerController?()
+    
+    var images = [UIImage]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     @IBOutlet weak var closeButton: UIButton!{
         didSet{
@@ -25,8 +29,8 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        imagePicker?.delegate=self
-
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     @IBAction func goToCameraRoll(sender: AnyObject) {
@@ -80,8 +84,9 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
        
         if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
             picker.dismissViewControllerAnimated(true, completion: nil)
-            //Implement if allowing user to edit the selected image
-            //let editedImage = info.objectForKey("UIImagePickerControllerEditedImage") as UIImage
+            images.append(image)
+            
+            self.collectionView.reloadData()
         }
     }
     
@@ -103,4 +108,68 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
      // Pass the selected object to the new view controller.
      }
      */
+}
+
+
+
+
+
+
+extension PhotoViewController : UICollectionViewDataSource {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(Constants.ReuseIDs.photoCell,forIndexPath:indexPath) as! CustomCollectionViewCell
+        
+        
+        let image  = self.images[indexPath.item]
+        cell.imageView.image = image
+        
+//        cell.caption.text = "This is some caption text"
+
+        
+        return cell
+    }
+    
+//    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+//        
+//        let headerView: FruitsHeaderView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: headerViewIdentifier, forIndexPath: indexPath) as! FruitsHeaderView
+//        
+//        headerView.sectionLabel.text = dataSource.gettGroupLabelAtIndex(indexPath.section)
+//        
+//        return headerView
+//    }
+}
+
+// MARK:- UICollectionViewDelegate Methods
+
+extension PhotoViewController : UICollectionViewDelegate {
+    
+//    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+//        highlightCell(indexPath, flag: true)
+//    }
+//    
+//    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+//        highlightCell(indexPath, flag: false)
+//    }
+}
+
+extension PhotoViewController: UICollectionViewDelegateFlowLayout {
+    // MARK:- UICollectioViewDelegateFlowLayout methods
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        // http://stackoverflow.com/questions/28872001/uicollectionview-cell-spacing-based-on-device-sceen-size
+        
+        let length = (UIScreen.mainScreen().bounds.width-15)/2
+        return CGSizeMake(length,length);
+    }
 }
